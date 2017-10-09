f3d7489b89d2e44f380189a0bb5015673de0a2b5
Daniel Tiefenauer <git@tiefenauer.info>
2017-10-09 18:04:03 +0200

    improved filtering of relevant tags

--- src/extractor/preproc.py
+++ src/extractor/preproc.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import re
 
+# only these tags will be considered
 RELEVANT_TAGS = ['p', 'h1', 'h2', 'h3', 'span', 'div', 'title', 'ul', 'ol']
+# these tags will be filtered out without replacement
 IRRELEVANT_TAGS = ['meta', 'link', 'style', 'script', 'noscript', 'iframe', 'form', 'input', 'img']
 
 
 def extract_relevant_tags(record):
     soup = BeautifulSoup(record['contentbytes'], 'html.parser')
-    # writeToFile(soup)
     extracted_tags = []
     extract_tags(soup, extracted_tags)
     return extracted_tags
@@ -19,26 +20,17 @@ def print_tags(tags):
 
 
 def extract_tags(tag, tags=[]):
-    if has_children(tag):
+    if contains_relevant_children(tag):
+        # nested tag
         for el in (el for el in tag.children if el.string != '\n'):
             extract_tags(el, tags)
         return
 
+    # leaf tag
     if is_relevant(tag) and tag not in tags:
         tag = strip_tag(tag)
         if has_content(tag):
             tags.append(tag)
-
-
-def move_up(tags):
-    all_tags = []
-    for tag in tags:
-        parent = tag.parent
-        all_tags.append(tag.extract())
-        for irr_tag in parent.findAll(IRRELEVANT_TAGS):
-            irr_tag.extract()
-        all_tags.append(parent)
-    return all_tags
 
 
 def is_relevant(tag, regexes=RELEVANT_TAGS):
@@ -47,34 +39,41 @@ def is_relevant(tag, regexes=RELEVANT_TAGS):
     return tag.name and re.match(combined, tag.name)
 
 
-def has_children(tag):
-    if hasattr(tag, 'findAll'):
-        relevant_children = tag.findAll(RELEVANT_TAGS)
+def contains_relevant_children(el):
+    if hasattr(el, 'findAll'):
+        # el is a Tag with possible nested tags
+        relevant_children = el.findAll(RELEVANT_TAGS)
         return len(relevant_children) > 0
-    return hasattr(tag, 'children') \
-           and len(list(tag.children)) > 0
+    # el is no Tag
+    return contains_children(el)
 
 
-def has_descendants(tag):
-    return hasattr(tag, 'descendants') \
-           and len(list(tag.descendants)) > 1
+def contains_children(tag):
+    return hasattr(tag, 'children') and len(list(tag.children)) > 0
 
 
-def strip_tags(tags):
-    stripped_tags = []
-    for tag in tags:
-        stripped = tag.getText(strip=True)
-        if len(stripped) > 0:
-            tag.string = stripped
-            stripped_tags.append(tag)
-    return stripped_tags
+def contains_descendants(tag):
+    return hasattr(tag, 'descendants') and len(list(tag.descendants)) > 0
 
 
 def strip_tag(tag):
-    stripped = tag.getText(strip=True)
+    # this will not work because inner markup such as <strong> would be destroyed!
+    stripped = tag.getText(strip=True, separator=' ')
     if len(stripped) > 0:
         tag.string = stripped
     return tag
+
+    #TODO: Find better version
+    # stripped = re.sub('\s+', ' ', str(tag.getText()))
+    #whitespaces = re.compile(r'[^a-zA-Z\d\s:]')
+    #[el.text.replace('\r', ' ').replace('\n', ' ').replace(' ', '') for el in tag.findAll() if el.name != 'br']
+    # for innerEl in [el for el in tag.findAll()]:
+    #     if innerEl.name == 'br':
+    #         innerEl.extract()
+    #     else:
+    #         stripped = innerEl.text.replace('\r', '').replace('\n', '').strip()
+    #         innerEl.string = stripped
+    # return tag
 
 
 def has_content(tag):
