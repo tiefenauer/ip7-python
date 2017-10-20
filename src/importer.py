b6855df54afdb990cb8222b21668b5fe2d415848
Daniel Tiefenauer <git@tiefenauer.info>
2017-10-20 07:35:25 +0200

    using tsv instead of csv

--- src/importer.py
+++ src/importer.py
@@ -40,7 +40,7 @@ class FetchflowImporter(object):
 
 class JobNameImporter(object):
     def __init__(self):
-        self.job_names = pandas.read_csv('../resource/tbfulljob_DE.csv', delimiter=';', names=['job_name'])
+        self.job_names = pandas.read_csv('../resource/tbfulljob_DE.tsv', delimiter=';', names=['job_name'])
 
     def __iter__(self):
         for job_name in self.job_names['job_name']:
