88b3b578eb1081516530df67446f4ab3516812f5
Daniel Tiefenauer <git@tiefenauer.info>
2017-10-23 20:40:58 +0200

    add x28-importer
    and do some refactoring

---
+++ src/importer/x28_importer.py
@@ -0,0 +1,52 @@
+import logging
+import os
+
+import sys
+
+from src import db
+from src.db import Database
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+class X28Importer(object):
+    def __init__(self, dirname='D:/db/x28'):
+        self.dirname = dirname
+        self.num_files = len([name for name in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, name))])
+
+    def __enter__(self):
+        self.conn = db.connect_to(Database.X28)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+
+    def __iter__(self):
+        logging.info('Processing {} files...'.format(self.num_files))
+        for fname in os.listdir(self.dirname):
+            with open(os.path.join(self.dirname, fname), encoding='utf-8') as file:
+                yield file.read()
+
+    def insert(self, jsonobj):
+        x28_id = jsonobj['id']
+        title = jsonobj['title']
+        html = jsonobj['htmlcontent']
+        plaintext = jsonobj['plaincontent']
+        url = jsonobj['url']
+        cursor = self.conn.cursor()
+        sql = r"""INSERT INTO labeled_jobs(x28_id, html, plaintext, url, title) 
+                          VALUES (%s,%s,%s,%s,%s)
+                          ON CONFLICT(x28_id) DO UPDATE
+                          SET html = excluded.html,
+                              plaintext = excluded.plaintext,
+                              url = excluded.url,
+                              title = excluded.title
+                              """
+        bla = cursor.mogrify(sql, (x28_id, html, plaintext, url, title))
+        cursor.execute(bla)
+        self.conn.commit()
+
+    def truncate_tables(self):
+        logging.info('truncating target tables...')
+        cursor = self.conn.cursor()
+        cursor.execute("""TRUNCATE labeled_jobs""")
