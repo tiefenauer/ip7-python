1c3e9293a095eebe417c2e4b0e6a868af9b73f90
Daniel Tiefenauer <git@tiefenauer.info>
2017-10-27 13:05:12 +0200

    add option to select only a single result add evaluator

--- src/evaluate_fulltextsearch.py
+++ src/evaluate_fulltextsearch.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 
 from src import preproc
 from src.classifier import classifier_jobtitle
+from src.evaluation.evaluate_tolerant import TolerantJobTitleClassificationEvaluator
 from src.importer.data_train import TrainingData
 
 logging.basicConfig(stream=sys.stdout, format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -13,7 +14,8 @@ logging.basicConfig(stream=sys.stdout, format='%(asctime)s : %(levelname)s : %(m
 parser = argparse.ArgumentParser(description="""
 Reads training data and classifies it using full text search.
 """)
-parser.add_argument('-w', '--write', action='store_false',
+parser.add_argument('id', nargs='?', type=int, help='(optional) single id to process')
+parser.add_argument('-w', '--write', action='store_true',
                     help="""Write classification results to DB (default=False). If set to true this will save the 
                     classification results in the DB. If set to false this script will only provide a live view
                     on the classifier's performance""")
@@ -22,30 +24,18 @@ parser.add_argument('-t', '--truncate', action='store_true',
 args = parser.parse_args()
 
 
-def evaluate_live(positives, negatives, actual_job_title, predicted_job_title):
-    if actual_job_title == predicted_job_title:
-        positives += 1
-    else:
-        negatives += 1
-    perf = positives / (positives + negatives)
-    return positives, negatives, perf
-
-
 if __name__ == '__main__':
-    with TrainingData() as data_train:
+    evaluator = TolerantJobTitleClassificationEvaluator()
+    with TrainingData(args.id) as data_train:
         if args.truncate:
             data_train.truncate_classification_tables()
         logging.info("Processing {} rows...".format(data_train.num_rows))
-        positives = 0
-        negatives = 0
-        performance = 0
         tqdm_data = tqdm(data_train, total=data_train.num_rows, unit=' rows')
         for row in (row for row in tqdm_data if row['html']):
             relevant_tags = preproc.preprocess(row['html'])
             (job_title, job_count) = classifier_jobtitle.find_best(relevant_tags)
+            evaluator.evaluate(row['title'], job_title)
             if job_title is not None:
-                (positives, negatives, performance) = evaluate_live(positives, negatives, row['title'], job_title)
-                desc = "positives={}, negatives={}, performance={}".format(positives, negatives, "{:1.4f}".format(performance))
-                tqdm_data.set_description(desc)
+                tqdm_data.set_description(evaluator.get_description())
                 if args.write:
                     data_train.classify_job(row['id'], job_title, job_count)
--- src/importer/data_train.py
+++ src/importer/data_train.py
@@ -8,11 +8,19 @@ logging.basicConfig(stream=sys.stdout, format='%(asctime)s : %(levelname)s : %(m
 
 
 class TrainingData(object):
+    def __init__(self, id):
+        self.id = id
+        if self.id is None:
+            self.id = -1000
+
     def __enter__(self):
         self.conn_read = db.connect_to(Database.X28)
         self.conn_write = db.connect_to(Database.X28)
         cursor = self.conn_read.cursor()
-        cursor.execute("SELECT count(*) as num_rows FROM labeled_jobs")
+        cursor.execute("""SELECT count(*) AS num_rows 
+                          FROM labeled_jobs 
+                          WHERE %(id)s < 0 OR id = %(id)s""",
+                       {'id': self.id})
         self.num_rows = cursor.fetchone()['num_rows']
         return self
 
@@ -22,13 +30,18 @@ class TrainingData(object):
 
     def __iter__(self):
         cursor = self.conn_read.cursor()
-        cursor.execute("""SELECT id, html, plaintext, url, title, x28_id FROM labeled_jobs""")
+        cursor.execute("""SELECT id, html, plaintext, url, title, x28_id 
+                          FROM labeled_jobs
+                          WHERE %(id)s < 0 OR id = %(id)s
+                          """,
+                       {'id': self.id}
+                       )
         for row in cursor:
             yield row
 
     def classify_job(self, job_id, job_name, job_count):
         cursor = self.conn_write.cursor()
-        cursor.execute("""INSERT into job_name_fts (job_id, job_name, job_count) 
+        cursor.execute("""INSERT INTO job_name_fts (job_id, job_name, job_count) 
                           VALUES(%s, %s, %s)""",
                        (job_id, job_name, job_count))
         self.conn_write.commit()
@@ -37,3 +50,4 @@ class TrainingData(object):
         logging.info('truncating target tables...')
         cursor = self.conn_read.cursor()
         cursor.execute("""TRUNCATE job_name_fts""")
+        self.conn_read.commit()
