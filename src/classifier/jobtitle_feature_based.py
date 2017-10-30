c460556d3e103100e2e2fab6b5a7a9e1d1c8f2ff
Daniel Tiefenauer <git@tiefenauer.info>
2017-10-30 15:51:25 +0100

    refactored feature-based classifier

--- src/classifier/jobtitle_feature_based.py
+++ src/classifier/jobtitle_feature_based.py
@@ -1,7 +1,14 @@
-from src.classifier import jobtitle_scorer as ranking
 from src.classifier.classification_strategy import ClassificationStrategy
 from src.importer.job_name_importer import JobNameImporter
 from src.jobtitle import jobtitle_extractor as extractor
+
+job_names = JobNameImporter()
+tag_weight = {
+    'h1': 0.6,
+    'h2': 0.3,
+    'h3': 0.1,
+    'default': 0.1
+}
 
 
 class FeatureBasedJobTitleClassification(ClassificationStrategy):
@@ -15,30 +22,41 @@ class FeatureBasedJobTitleClassification(ClassificationStrategy):
         Classification is then made by calculating a score based on the extracted features. The vacancy is classified
         as the job name with the highest occurrence from the tag with the highest score."""
 
-    def __init__(self):
-        self.job_names = JobNameImporter()
+    def __init__(self, job_names=job_names):
+        self.job_names = job_names
 
     def classify(self, tags):
         best_match = None
         num_occurrence = 0
         best_score = 0
-        for tag_features in (tag_features for tag_features in (self.extract_features(tag) for tag in tags) if
-                             tag_features['matches']):
-            score = ranking.calculate_score(tag_features)
+        for tag_features in (tag_features for tag_features in
+                             (self.extract_features(tag) for tag in tags)
+                             if tag_features['matches']):
+            score = self.calculate_score(tag_features)
             if score > best_score:
                 best_score = score
                 best_match, num_occurrence = next(iter(tag_features['matches']), (None, None))
         return best_match, num_occurrence, best_score
 
-    def extract_features(self, tag, job_names=None):
-        if job_names is None:
-            job_names = self.job_names
+    def extract_features(self, tag, job_names=job_names):
         job_titles = extractor.extract_job_titles(str(tag), job_names)
         return {
             'tag': tag.name if tag.name else None,
             'matches': sorted(list(job_titles), key=lambda match: (match[1], match[0]),
                               reverse=True) if job_titles else []
         }
+
+    def calculate_score(self, features):
+        rank = 0
+        key = features['tag'] if features['tag'] in tag_weight else 'default'
+        rank += tag_weight[key]
+        # todo: add more feature values here if available
+        return self.normalize(rank)
+
+    def normalize(self, score):
+        if score < 1:
+            return score
+        return 1 / score
 
     def title(self):
         return self.TITLE
