71dba6ad8a753327b3b7e4fb7d33d15280d9e0d5
Daniel Tiefenauer <git@tiefenauer.info>
2017-11-05 17:41:43 +0100

    add origin to known_jobs table

--- src/preprocessing/import_job_names.py
+++ src/preprocessing/import_job_names.py
@@ -1,31 +1,39 @@
+import logging
 import os
 import re
+import sys
 
 import nltk
 import pandas
+from tqdm import tqdm
 
 from src import db
 from src.db import Database
 from src.util import jobtitle_util
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 resource_dir = 'D:/code/ip7-python/resource'
 
 
 def import_job_names_from_file():
     filename = os.path.join(resource_dir, 'job_titles.tsv')
+    logging.info('importing job names from {}'.format(filename))
     df = pandas.read_csv(filename, delimiter='\t', names=['job_name'])
     return df['job_name']
 
 
 def import_job_name_from_fts():
-    cursor = conn.cursor(dictionary=True)
+    logging.info('importing job names from Full Text Search results')
+    cursor = conn.cursor()
     sql = """SELECT a.title actual, p.job_name AS prediction
             FROM job_name_fts p
             LEFT OUTER JOIN labeled_jobs a ON a.id = p.job_id"""
     cursor.execute(sql)
     for row in cursor:
-        job_name = merge(row['actual'], row['prediction'])
-        yield job_name
+        job_name, origin = merge(row['actual'], row['prediction'])
+        if job_name is not None:
+            yield job_name, origin
 
 
 def merge(actual, prediction):
@@ -33,27 +41,45 @@ def merge(actual, prediction):
     prediction_n = jobtitle_util.to_male_form(prediction)
     # full match
     if actual_n == prediction_n:
-        return actual_n
+        return actual_n, 'predicted'
     # partial match / compound word
     words_actual = nltk.word_tokenize(re.escape(actual_n), language='german')
     words_prediction = nltk.word_tokenize(prediction_n, language='german')
     if prediction_n.lower() in actual_n.lower():
         if len(words_actual) == len(words_prediction):
-            return actual
+            return actual, 'compound'
     # no match
     if len(words_actual) == 1:
-        return actual_n
-    return None
+        return actual_n, 'guessed'
+    return None, None
 
 
-def write_job_names_to_db(job_names):
-    pass
+def write_job_name_to_db(name, origin):
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO job_names (job_name, origin) VALUES (%s, %s)""", (name, origin))
+    conn.commit()
 
 
-conn = db.connect_to(Database.FETCHFLOW_PG)
+def truncate_target_table():
+    logging.info('truncating target tables')
+    cursor = conn.cursor()
+    cursor.execute("""TRUNCATE TABLE job_names""")
+    conn.commit()
+
+
+conn = db.connect_to(Database.X28_PG)
+
 if __name__ == '__main__':
-    job_names_from_file = import_job_names_from_file()
-    job_names_from_fts = import_job_name_from_fts()
-    write_job_names_to_db(job_names)
+    truncate_target_table()
+    new_jobs = set()
+    known_jobs = import_job_names_from_file()
+    for job_name in tqdm(known_jobs):
+        write_job_name_to_db(job_name, 'job_titles.tsv')
+        new_jobs.add(job_name)
+    fts_jobs = import_job_name_from_fts()
+    for job_name, origin in tqdm(fts_jobs):
+        if job_name not in new_jobs:
+            write_job_name_to_db(job_name, 'fts ' + origin)
+            new_jobs.add(job_name)
 
 conn.close()
--- test/preprocessing/test_import_job_names.py
+++ test/preprocessing/test_import_job_names.py
@@ -14,17 +14,17 @@ class TestImportJobNames(unittest.TestCase):
         prediction = 'Trockenbauer'
         # act
         result = testee.merge(actual, prediction)
-        assert_that(result, is_(actual))
+        assert_that(result, is_((actual, 'predicted')))
 
     def test_merge_different_forms_returns_male_form(self):
         job_name = 'Maler'
         for actual, prediction in itertools.combinations(jobtitle_util.create_variants(job_name), 2):
             message = '{} / {} should be merged to {}'.format(actual, prediction, job_name)
-            assert_that(testee.merge(actual, prediction), is_(job_name), message)
+            assert_that(testee.merge(actual, prediction), is_((job_name, 'predicted')), message)
 
     def test_merge_partial_match_returns_full(self):
-        assert_that(testee.merge('Möbelschreiner', 'Schreiner'), is_('Möbelschreiner'))
-        assert_that(testee.merge('Bauspengler', 'Spengler'), is_('Bauspengler'))
+        assert_that(testee.merge('Möbelschreiner', 'Schreiner'), is_(('Möbelschreiner', 'compound')))
+        assert_that(testee.merge('Bauspengler', 'Spengler'), is_(('Bauspengler', 'compound')))
 
     def test_marge_no_match_actual_is_known_job_returns_actual(self):
         actual_job_name = 'Polymechaniker'
@@ -33,11 +33,14 @@ class TestImportJobNames(unittest.TestCase):
         elektroniker_variants = jobtitle_util.create_variants(predicted_job_name)
 
         for actual_variant, prediction_variant in zip(polymechaniker_variants, elektroniker_variants):
-            assert_that(testee.merge(actual_variant, prediction_variant), is_(actual_job_name))
+            assert_that(testee.merge(actual_variant, prediction_variant), is_((actual_job_name, 'guessed')))
 
     def test_merge_no_match_actual_is_one_word_returns_actual(self):
-        assert_that(testee.merge('Fassadenisolateur', 'Gipser'), is_('Fassadenisolateur'))
-        assert_that(testee.merge('Fassadenisolateur (m/w)', 'Gipser'), is_('Fassadenisolateur'))
+        assert_that(testee.merge('Fassadenisolateur', 'Gipser'), is_(('Fassadenisolateur', 'guessed')))
+        assert_that(testee.merge('Fassadenisolateur (m/w)', 'Gipser'), is_(('Fassadenisolateur', 'guessed')))
 
     def test_merge_no_match_actual_is_compound_word_returns_actual(self):
-        assert_that(testee.merge('Kupfer-Spleisser', 'Monteur'), is_('Kupfer-Spleisser'))
+        assert_that(testee.merge('Kupfer-Spleisser', 'Monteur'), is_(('Kupfer-Spleisser', 'guessed')))
+
+    def test_merge_no_match_unmergeable_returns_None(self):
+        assert_that(testee.merge('Projektleiter Sanitär', 'Techniker'), is_(None))
