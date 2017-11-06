508318308e69c06d138275957c820ed516a4565d
Daniel Tiefenauer <git@tiefenauer.info>
2017-11-06 16:38:35 +0100

    add test

--- test/provider/test_job_class_provider.py
+++ test/provider/test_job_class_provider.py
@@ -1,7 +1,7 @@
 import collections
 import unittest
 
-from hamcrest import assert_that, instance_of, is_, not_, empty
+from hamcrest import assert_that, instance_of, is_, not_, empty, greater_than
 
 from src.provider.job_class_provider import JobClassProvider
 
@@ -10,6 +10,7 @@ class TestJobClassProvider(unittest.TestCase):
     def test_is_iterable_and_closeable(self):
         with JobClassProvider() as testee:
             assert_that(testee, is_(instance_of(collections.Iterable)))
+            assert_that(len(list(testee)), is_(greater_than(0)))
             for item in testee:
                 assert_that(item['id'], is_(not_(empty())))
                 assert_that(item['job_class'], is_(not_(empty())))
