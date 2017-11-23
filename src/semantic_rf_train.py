df539d7d81e6d71d58563f4409f35c278c2051b5
Daniel Tiefenauer <git@tiefenauer.info>
2017-11-23 16:51:05 +0100

    zwischenprodukt

--- src/semantic_rf_train.py
+++ src/semantic_rf_train.py
@@ -1,26 +1,20 @@
 import argparse
 
 from src.classifier.semantic_classifier_rf import SemanticClassifierRF
-from src.database.FetchflowTrainData import FetchflowTrainData
 from src.database.X28TrainData import X28TrainData
-from src.preprocessing.preprocessor_semantic import SemanticX28Preprocessor
 from src.util.boot_util import log_setup
 
 log_setup()
 
 parser = argparse.ArgumentParser(description="""Train Semantic Classifier (Word2Vec)""")
 parser.add_argument('source', nargs='?', choices=['fetchflow', 'x28'], default='fetchflow')
-parser.add_argument('-s', '--split', nargs='?', type=float, default=1.0,
+parser.add_argument('-s', '--split', nargs='?', type=float, default=0.001,
                     help='(optional) fraction value of labeled data to use for training')
 parser.add_argument('-m', '--model',
                     help='(optional) file with saved model to use. A new model will be created if not set.')
 args = parser.parse_args()
 
-data_train = FetchflowTrainData(args)
-if args.source == 'x28':
-    data_train = X28TrainData(args)
-
+data_train = X28TrainData(args)
 classifier = SemanticClassifierRF(args.model)
-
 if __name__ == '__main__':
     classifier.train_model(data_train)
