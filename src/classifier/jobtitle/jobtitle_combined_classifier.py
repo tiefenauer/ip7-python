1134a91471871c9c5dc2360ca29e0777fb1c932e
Daniel Tiefenauer <git@tiefenauer.info>
2017-12-11 15:01:31 +0100

    add some explanatory comment

--- src/classifier/jobtitle/jobtitle_combined_classifier.py
+++ src/classifier/jobtitle/jobtitle_combined_classifier.py
@@ -3,6 +3,9 @@ from src.classifier.tag_classifier import TagClassifier
 
 
 class CombinedJobtitleClassifier(TagClassifier, JobtitleClassifier):
+    """Classifies a vacancy by combining several methods (FTS, semantic information, structural information
+    and HTML tags). The classifier"""
+
     def classify(self, tags):
         pass
 
