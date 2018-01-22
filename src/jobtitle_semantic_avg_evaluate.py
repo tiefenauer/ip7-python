90bb2df7882bee3cb5b4e8f152ed3fa03e5631ca
Daniel Tiefenauer <git@tiefenauer.info>
2018-01-22 14:43:44 +0100

    evaluate Wor2Vec Model with semantic testset for relation job-workplace

--- src/jobtitle_semantic_avg_evaluate.py
+++ src/jobtitle_semantic_avg_evaluate.py
@@ -1,13 +1,9 @@
 import argparse
 import logging
 
-from pony.orm import commit, db_session
-from tqdm import tqdm
-
 from src.classifier.jobtitle.jobtitle_semantic_classifier_avg import JobtitleSemanticClassifierAvg
 from src.database.X28TestData import X28TestData
-from src.database.entities_pg import Job_Class, Job_Class_Similar, Job_Class_To_Job_Class_Similar, \
-    Semantic_Avg_Classification_Results
+from src.database.entities_pg import Semantic_Avg_Classification_Results
 from src.evaluation.jobtitle.evaluator_jobtitle_semantic_avg import SemanticAVGEvaluation
 from src.preprocessing.semantic_preprocessor import SemanticPreprocessor
 from src.util.log_util import log_setup
@@ -33,45 +29,8 @@ parser.add_argument('-w', '--write', action='store_true',
 
 args = parser.parse_args()
 
-
-def doesnt_match(words):
-    print('Doesn\'t match: ' + words)
-    result = model.doesnt_match(words.split())
-    print(result)
-
-
-def most_similar(word):
-    print('Most similar: ' + word)
-    row_format = "{:>30} {}"
-    for result, score in model.most_similar(word):
-        print(row_format.format(result, score))
-
-
-def update_most_similar_job_classes():
-    log.info('update_most_similar_job_classes: Updating DB with most similar jobs for trained jobs...')
-    with db_session:
-        # truncate previous mappings
-        Job_Class_To_Job_Class_Similar.select().delete(bulk=True)
-        Job_Class_Similar.select().delete(bulk=True)
-        commit()
-        # add new mappings
-        known_and_trained_jobs = list(job_class for job_class in Job_Class.select()
-                                      if job_class.job_name in model.index2word)
-
-        for job_class in tqdm(known_and_trained_jobs, unit=' rows'):
-            for similar_name, score in model.most_similar(job_class.job_name):
-                if Job_Class_Similar.exists(job_name_similar=similar_name):
-                    job_class_similar = Job_Class_Similar.get(job_name_similar=similar_name)
-                else:
-                    job_class_similar = Job_Class_Similar(job_name_similar=similar_name)
-                commit()
-                Job_Class_To_Job_Class_Similar(fk_job_class=job_class.id, fk_job_class_similar=job_class_similar.id,
-                                               score=score)
-    log.info('update_most_similar_job_classes: done!')
-
-
 if args.model == 'fetchflow':
-    args.model = 'semantic_fetchflow_x28.gz'
+    args.model = 'semantic_avg_x28.gz'
     Semantic_Avg_Classification_Results._discriminator_ += '-fetchflow'
 else:
     args.model = 'semantic_avg_x28.gz'
